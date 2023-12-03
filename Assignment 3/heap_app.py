c013d5eaa6afd4ef54b5eef70a0b23498f104300
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-12-03 19:39:00 +0000

    Update heap_app.py

--- Assignment 3/heap_app.py
+++ Assignment 3/heap_app.py
@@ -1,6 +1,4 @@
-
 class Heap:
-
     values: list = [0]
     last_index: int = 0
 
@@ -8,22 +6,26 @@ class Heap:
     def increase_array_size(self, new_array_size: int) -> None:
         for index in range(len(self.values), new_array_size):
             self.values.append(0)
-    
+
     # up heaps value in given index
     def up_heap(self, x: int):
         parent_index = self.find_parent(x)
-        while (parent_index != 0 and self.values[x] > self.values[parent_index]):
+        while parent_index != 0 and self.values[x] > self.values[parent_index]:
             # swapping values
-            self.values[parent_index], self.values[x] = self.values[x], self.values[parent_index]
-    
+            self.values[parent_index], self.values[x] = (
+                self.values[x],
+                self.values[parent_index],
+            )
+
     # increases array size then inserts the new value, and up heaps
     def insert(self, x: int) -> None:
         self.last_index += 1
         if self.last_index >= len(self.values):
             self.increase_array_size((len(self.values) * 2))
+
         self.values[self.last_index] = x
         self.up_heap(self.last_index)
-    
+
     # parent at floor division 2 of index given
     def find_parent(self, x: int) -> int:
         return x // 2
@@ -31,7 +33,7 @@ class Heap:
     # returns the left and right child indexes
     def find_child(self, x: int) -> tuple:
         return (x * 2, (x * 2) + 1)
-    
+
     # finds smallest value, swaps with last value and up heaps
     def delete_small(self) -> None:
         starting_index: int = self.find_parent(self.last_index) + 1
@@ -40,12 +42,15 @@ class Heap:
 
         # loops through all leaf nodes
         for index in range(starting_index + 1, self.last_index):
-            if (self.values[index] < smallest_value):
+            if self.values[index] < smallest_value:
                 smallest_value = self.values[index]
                 smallest_value_index = index
 
         # swapping values
-        self.values[smallest_value_index], self.values[self.last_index] = self.values[self.last_index], self.values[smallest_value_index]
+        self.values[smallest_value_index], self.values[self.last_index] = (
+            self.values[self.last_index],
+            self.values[smallest_value_index],
+        )
 
         # delete value
         self.values[self.last_index] = 0
