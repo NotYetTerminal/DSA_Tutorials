3c9adb64f1bb2ccdca7fc87c7a7cc03441e3d921
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-12-03 20:58:10 +0000

    Update heap_app.py

--- Assignment 3/heap_app.py
+++ Assignment 3/heap_app.py
@@ -1,5 +1,7 @@
 class Heap:
+    # values is used as an array
     values: list = [0]
+    # last_index is the position where the last value is
     last_index: int = 0
 
     # increases the values size to the new array size
@@ -41,7 +43,7 @@ class Heap:
         smallest_value_index: int = starting_index
 
         # loops through all leaf nodes
-        for index in range(starting_index + 1, self.last_index):
+        for index in range(starting_index + 1, self.last_index + 1):
             if self.values[index] < smallest_value:
                 smallest_value = self.values[index]
                 smallest_value_index = index
@@ -58,6 +60,64 @@ class Heap:
 
         self.up_heap(smallest_value_index)
 
+    # prints the heap graphically
+    def pretty_print_heap(self) -> None:
+        print_lines_list: list = []
+        # keeps track of multiples of 2
+        current_multiple: int = 1
+        longest_number_length: int = 0
+        counter: int = 1
+
+        # organises values to print out
+        for index in range(1, self.last_index + 1):
+            if index == current_multiple:
+                print_lines_list.append([' ', str(self.values[index])])
+                current_multiple *= 2
+                counter = counter * 2 + 1
+            else:
+                # add to last line
+                print_lines_list[-1].append(' ')
+                print_lines_list[-1].append(self.values[index])
+            if longest_number_length < len(str(self.values[index])):
+                longest_number_length = len(str(self.values[index]))
+        
+        counter //= 2
+        other_counter: int = len(print_lines_list)
+        for line in print_lines_list:
+            # prints out markers whether each node is a left or right child
+            if len(line) != 2:
+                left: bool = True
+                for index in range(len(line)):
+                    if line[index] == ' ':
+                        if index == 0:
+                            print(' ' * (counter * longest_number_length - (2 ** other_counter)), end='')
+                        else:
+                            print(' ' * (counter * longest_number_length), end='')
+                    else:
+                        print(' ' * (longest_number_length - 1), end='')
+                        if left:
+                            print('/', end='')
+                        else:
+                            print('\\', end='')
+                        left = not left
+                print()
+            
+            # prints out values
+            for index in range(len(line)):
+                if line[index] == ' ':
+                    if index == 0:
+                        print(' ' * (counter * longest_number_length - (2 ** other_counter)), end='')
+                    else:
+                        print(' ' * (counter * longest_number_length), end='')
+                else:
+                    print(' ' * (longest_number_length - len(str(line[index]))), end='')
+                    print(line[index], end='')
+            print()
+            counter //= 2
+            other_counter -= 1
+
+
+
 
 def main():
     heap: Heap = Heap()
@@ -66,13 +126,26 @@ def main():
     heap.insert(5)
     heap.insert(15)
     heap.insert(16)
+    heap.insert(22)
+    heap.insert(10)
+    heap.insert(11)
     heap.insert(4)
     heap.insert(2)
+    heap.insert(4)
+    heap.insert(2)
+    heap.insert(10)
+    heap.insert(51)
+    heap.insert(20)
+    heap.insert(5)
+    heap.insert(15)
+    heap.insert(16)
+    heap.insert(4)
+    heap.insert(12)
     heap.insert(10)
     heap.insert(11)
     print(heap.values)
     heap.delete_small()
-    print(heap.values)
+    heap.pretty_print_heap()
     pass
 
 
